8ffdfa773682f4cc4997ff627f3f90519fe96658
Karina <karinadesyatnik27@gmail.com>
2023-09-12 23:01:57 +0300

    delivery and pick up pages

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,59 @@ $(function(){
         $('.login-modal').fadeIn();
     })
 
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true
+    });
+
+    $('.proceed-item .green-button').click(function() {
+        $(this).parent().addClass('active');
+    })
+
+    $('.close-item').click(function(){
+        $('.proceed-item').removeClass('active');
+    })
+
+    $('.code-button').click(function(){
+        $('.proceed-item__content-wrapper').addClass('active');
+    })
+
+    $('.pick-up-select').on('change', function() {
+       $('.pick-up__content').addClass('active');
+      });
+
+    
+
       
 
 });
+
+const incrementButton = document.querySelector("#increment");
+const decrementButton = document.querySelector("#decrement");
+const quantityInput = document.querySelector("#quantity");
+
+incrementButton.addEventListener("click", () => {
+    if(quantityInput.value <= 14) {
+  quantityInput.value = parseInt(quantityInput.value) + 1;
+    }
+});
+decrementButton.addEventListener("click", () => {
+    if(quantityInput.value >= 1) {
+        quantityInput.value = parseInt(quantityInput.value) - 1;
+    }
+  
+});
+
+let codeInput = document.getElementById('code-input')
+
+codeInput.oninput = function() {
+    if (codeInput.value.length >= 4) {
+        document.getElementById('codeBtn').classList.add('active');
+    } else {
+        document.getElementById('codeBtn').classList.remove('active');
+    }
+    
+  };
+
+
