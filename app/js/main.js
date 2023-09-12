0b39650328d001896f7fa5e599f9d77c9ccde45f
Karina <karinadesyatnik27@gmail.com>
2023-09-12 16:07:16 +0300

    login modal

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,25 @@
 $(function(){
 
+    $('.close-modal').click(function(){
+        $('.login-modal').fadeOut();
+    })
 
+    $(function($){
+        $(document).mouseup(function (e){ 
+          var div = $(".login-modal-content"); 
+          var div2 = $(".login-modal");
+          if (!div.is(e.target) 
+              && div.has(e.target).length === 0) {
+            div2.fadeOut();
+      
+          }
+        });
+      });
+    
+    $('.header__login').click(function(){
+        $('.login-modal').fadeIn();
+    })
+
+      
 
 });
